d8519520f9654805b7b42eb64c5d63fde5bdeade
Swainson Holness <swainson.holness@gmail.com>
2025-06-13 17:07:00 -0700

    FASE project

--- ops.py
+++
@@ -1,8 +0,0 @@
-# ops.py
-class VirtualOp:
-    """Wrap an existing atomic op so it has its own identity/name."""
-    def __init__(self, base_op, name: str):
-        self.base_op = base_op
-        self.name = name
-    def __call__(self, X):
-        return self.base_op(X)
